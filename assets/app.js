73eb8d50da16729c37189387558a4c909dbaa296
NicoDestephen <ndestephen@gmail.com>
2024-03-30 18:51:47 +0100

    start about section

--- assets/app.js
+++ assets/app.js
@@ -6,3 +6,4 @@
  */
 import './styles/app.scss';
 import './loaded';
+import './slideButton';
---
+++ assets/slideButton.js
@@ -0,0 +1,5 @@
+var slideButton = document.getElementById('bouton-slide');
+    slideButton.addEventListener('click', function() {
+        console.log('coucou');
+    });
+
