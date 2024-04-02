d4721ffc5d353cfc1c4ae8836f831f7c47e03e18
NicoDestephen <ndestephen@gmail.com>
2024-04-02 13:50:46 +0200

    buttonSlide script works. When i click on it the home page body is hidden and the about page is display

--- assets/slideButton.js
+++ assets/slideButton.js
@@ -1,5 +1,10 @@
 var slideButton = document.getElementById('bouton-slide');
     slideButton.addEventListener('click', function() {
-        console.log('coucou');
+        var homePageBody = document.getElementById('home-page-body');
+        var aboutPage = document.getElementById('about-page');
+        homePageBody.classList.add('slide-up');
+        aboutPage.classList.remove('hidden');
+        setTimeout(function() {
+            aboutPage.classList.add('slide-up');
+        }, 3);
     });
-
