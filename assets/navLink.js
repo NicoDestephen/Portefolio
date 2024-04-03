ffe71257a9035a1753d31aecd6f201982338720b
NicoDestephen <ndestephen@gmail.com>
2024-04-03 23:14:05 +0200

    fix slide up and down for homePageBody and aboutPage

--- assets/navLink.js
+++ assets/navLink.js
@@ -1,31 +1,35 @@
 var aboutLink = document.querySelector('.about-link');
 var homeLink = document.querySelector('.home-link');
-
-if (aboutLink) {
-    aboutLink.addEventListener('click', function(about) {
-        about.preventDefault();
-        var homePageBody = document.getElementById('home-page-body');
-        var aboutPage = document.getElementById('about-page');
-        homePageBody.classList.add('slide-up');
-        aboutPage.classList.remove('hidden');
-        setTimeout(function() {
-            aboutPage.classList.add('slide-up');
-            document.title = 'Portefolio DESTEPHEN - A propos';
-        }, 3);
-    });
-}
+var homePageBody = document.getElementById('home-page-body');
+var aboutPage = document.getElementById('about-page');
 
 if (homeLink) {
     homeLink.addEventListener('click', function(home) {
         home.preventDefault();
-        var homePageBody = document.getElementById('home-page-body');
-        var aboutPage = document.getElementById('about-page');
+
         aboutPage.classList.add('slide-down');
-        // aboutPage.classList.add('hidden');
         homePageBody.classList.remove('hidden');
+
         setTimeout(function() {
             homePageBody.classList.add('slide-down');
+            aboutPage.classList.remove('slide-up');
             document.title = 'Portefolio DESTEPHEN - Home';
+        }, 3);
+    });
+}
+
+if (aboutLink) {
+    aboutLink.addEventListener('click', function(about) {
+        about.preventDefault();
+
+        homePageBody.classList.add('slide-up');
+        aboutPage.classList.remove('slide-down');
+        aboutPage.classList.remove('hidden');
+        
+        setTimeout(function() {
+            aboutPage.classList.add('slide-up');
+            homePageBody.classList.remove('slide-down');
+            document.title = 'Portefolio DESTEPHEN - A propos';
         }, 3);
     });
 }
--- assets/slideButton.js
+++ assets/slideButton.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var slideButton = document.getElementById('bouton-slide');
-    var pageTitle = document.title;
 
     slideButton.addEventListener('click', function() {
         var homePageBody = document.getElementById('home-page-body');
         var aboutPage = document.getElementById('about-page');
+        
         homePageBody.classList.add('slide-up');
+        homePageBody.classList.remove('slide-down');
+
+        aboutPage.classList.remove('slide-down');
         aboutPage.classList.remove('hidden');
+        
         setTimeout(function() {
             aboutPage.classList.add('slide-up');
             document.title = 'Portefolio DESTEPHEN - A propos';
