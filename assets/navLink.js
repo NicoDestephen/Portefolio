250f1a59eb0e4b084d6c5f1252162dee10b5a835
NicoDestephen <ndestephen@gmail.com>
2024-04-11 19:13:40 +0200

    hard update of navlink script + create contact page

--- assets/navLink.js
+++ assets/navLink.js
@@ -1,259 +1,186 @@
-var homeLink = document.querySelector('.home-link');
-var aboutLink = document.querySelector('.about-link');
-var projectLink = document.querySelector('.project-link');
+// var homeLink = document.querySelector('.home-link');
+// var aboutLink = document.querySelector('.about-link');
+// var projectLink = document.querySelector('.project-link');
 
-var homePageBody = document.getElementById('home-page-body');
-var aboutPage = document.getElementById('about-page');
-var projectPage = document.getElementById('project-page');
+// var homePage = document.getElementById('home-page-body');
+// var aboutPage = document.getElementById('about-page');
+// var projectPage = document.getElementById('project-page');
 
-var homeClicked = false;
-var aboutClicked = false;
-var projectClicked = false;
+// var homeClicked = false;
+// var aboutClicked = false;
+// var projectClicked = false;
 
-function homeResetCSS() {
-    navbar.classList.remove('homeLink-clicked');
-    navbar.classList.remove('aboutLink-clicked');
-    navbar.classList.remove('projectLink-clicked');
+// var userHistory = [];
 
-    homePageBody.classList.remove('hidden');
-    homePageBody.classList.remove('slide-down');
-    homePageBody.classList.remove('slide-up');
+// function updateUserHistory(page) {
+//     userHistory.push(page);
+// }
 
-    aboutPage.classList.remove('hidden');
-    aboutPage.classList.remove('slide-down');
-    aboutPage.classList.remove('slide-down2');
-    aboutPage.classList.remove('slide-up');
-    aboutPage.classList.remove('slide-up2');
+// function resetUserHistory() {
+//     userHistory = [];
+// }
 
+// function resetCSS() {
+//     navbar.classList.remove('homeLink-clicked', 'aboutLink-clicked', 'projectLink-clicked');
+//     homePage.classList.remove('hidden', 'slide-down', 'slide-up');
+//     aboutPage.classList.remove('hidden', 'slide-down', 'slide-down2', 'slide-up', 'slide-up2');
+//     projectPage.classList.remove('hidden', 'slide-down', 'slide-down2', 'slide-down3', 'slide-up', 'slide-up2', 'slide-up3');
+// }
 
-    projectPage.classList.remove('hidden');
-    projectPage.classList.remove('slide-down');
-    projectPage.classList.remove('slide-down2');
-    projectPage.classList.remove('slide-down3');
-    projectPage.classList.remove('slide-up');
-    projectPage.classList.remove('slide-up2');
-}
+// if (homeLink) {
+//     homeLink.addEventListener('click', function(home) {
+//         home.preventDefault();
+//         updateUserHistory('home');
+//         resetCSS();
 
-function aboutResetCSS() {
-    navbar.classList.remove('homeLink-clicked');
-    navbar.classList.remove('aboutLink-clicked');
-    navbar.classList.remove('projectLink-clicked');
+//         var clickedAbout = userHistory[userHistory.length - 2] === 'about';
+//         var clickedProject = userHistory[userHistory.length - 2] === 'project';
 
-    homePageBody.classList.remove('hidden');
-    homePageBody.classList.remove('slide-down');
-    aboutPage.classList.remove('slide-down2');
-    homePageBody.classList.remove('slide-up');
-    aboutPage.classList.remove('slide-up2');
+//         if (clickedAbout) {
+//             console.log('je clique sur home après avoir cliqué sur about');
+//             homePage.classList.remove('hidden');
+//             homePage.classList.add('slide-down');
+//             aboutPage.classList.add('slide-down');
+//             projectPage.classList.add('slide-down');
+//             projectPage.classList.remove('slide-down3');
+//             setTimeout(function() {
+//                 aboutPage.classList.add('hidden');
+//                 projectPage.classList.add('hidden');
+//                 navbar.classList.add('homeLink-clicked');
+//             }, 1000);
+//         } else if (clickedProject) {
+//             console.log('je clique sur home après avoir cliqué sur project');
+//             homePage.classList.remove('hidden');
+//             aboutPage.classList.add('hidden');
+//             homePage.classList.add('slide-down');
+//             projectPage.classList.add('slide-down');
+//             setTimeout(function() {
+//                 document.title = 'Portefolio DESTEPHEN - Home';
+//                 setTimeout(function() {
+//                     setTimeout(function() {
+//                         projectPage.classList.add('hidden');
+//                         homePage.classList.remove('slide-down');
+//                         projectPage.classList.remove('slide-down');
+//                         navbar.classList.add('homeLink-clicked');
+//                     }, 1000);                
+//                 }, 1000);                         
+//             }, 3);    
+//         } else {
+//             console.log('je clique sur home directement');
+//             homePage.classList.remove('hidden');
+//             aboutPage.classList.add('hidden');
+//             projectPage.classList.add('hidden');
+//             navbar.classList.add('homeLink-clicked');
+//         }
 
-    aboutPage.classList.remove('hidden');
-    aboutPage.classList.remove('slide-down');
-    aboutPage.classList.remove('slide-up');
+//         console.log(userHistory);
+//     });
+// }
 
-    projectPage.classList.add('hidden');
-    projectPage.classList.remove('slide-down');
-    projectPage.classList.remove('slide-down2');
-    projectPage.classList.remove('slide-down3');
-    projectPage.classList.remove('slide-up');
-    projectPage.classList.remove('slide-up2');
-}
+// if (aboutLink) {
+//     aboutLink.addEventListener('click', function(about) {
+//         about.preventDefault();
+//         updateUserHistory('about');
+//         resetCSS();
 
-function projectResetCSS() {
-    navbar.classList.remove('homeLink-clicked');
-    navbar.classList.remove('aboutLink-clicked');
-    navbar.classList.remove('projectLink-clicked');
+//         var clickedHome = userHistory[userHistory.length - 2] === 'home';
+//         var clickedProject = userHistory[userHistory.length - 2] === 'project';
 
-    homePageBody.classList.remove('hidden');
-    homePageBody.classList.remove('slide-down');
-    homePageBody.classList.remove('slide-up');
+//         if (clickedHome) {
+//             console.log('je clique sur about après avoir cliqué sur home');
+//             homePage.classList.add('slide-up');
 
-    aboutPage.classList.add('hidden');
-    aboutPage.classList.remove('slide-down');
-    aboutPage.classList.remove('slide-down2');
-    aboutPage.classList.remove('slide-up');
-    aboutPage.classList.remove('slide-up2');
+//             setTimeout(function() {
+//                 aboutPage.classList.add('slide-up'); 
+//                 projectPage.classList.add('hidden');
+//                 document.title = 'Portefolio DESTEPHEN - A propos';
+//                 navbar.classList.add('aboutLink-clicked');
+//             }, 3);
+//         } else if (clickedProject) {
+//             console.log('je clique sur about après avoir cliqué sur project');
+//             projectPage.classList.remove('slide-up3');
+//             projectPage.classList.remove('hidden');
+//             projectPage.classList.add('slide-down3');
+//             homePage.classList.add('hidden');
+//             homePage.classList.add('slide-up');
+//             aboutPage.classList.add('slide-up2');
+//             setTimeout(function() {
+//                 aboutPage.classList.add('slide-down2');
+//                 document.title = 'Portefolio DESTEPHEN - A propos';
+//                 setTimeout(function() {
+//                     aboutPage.classList.remove('hidden');
+//                     aboutPage.classList.remove('slide-up2');
+//                     projectPage.classList.add('hidden');
+//                     navbar.classList.add('aboutLink-clicked');
+//                     projectPage.classList.remove('slide-down3');
+//                 }, 1000);                   
+//             }, 3);
+//         } else {
+//             console.log('je clique sur about directement');
+//             projectPage.classList.add('hidden');
+//             homePage.classList.add('slide-up');
+//             setTimeout(function() {
+//                 aboutPage.classList.add('slide-up');
+//                 navbar.classList.add('aboutLink-clicked');
+//             }, 3);
+//         }
 
-    projectPage.classList.remove('hidden');
-    projectPage.classList.remove('slide-down');
-    projectPage.classList.remove('slide-down2');
-    projectPage.classList.remove('slide-down3');
-    projectPage.classList.remove('slide-up');
-    projectPage.classList.remove('slide-up2');
-}
+//         console.log(userHistory);
+//     });
+// }
 
-if (homeLink) {
-    homeLink.addEventListener('click', function(home) {
-        home.preventDefault();
+// if (projectLink) {
+//     projectLink.addEventListener('click', function(project) {
+//         project.preventDefault();
+//         updateUserHistory('project');
+//         resetCSS();
 
-        homeResetCSS();
+//         var clickedAbout = userHistory[userHistory.length - 2] === 'about';
+//         var clickedHome = userHistory[userHistory.length - 2] === 'home';
 
-        if (aboutClicked) {
-            console.log('je clique sur home apres avoir cliqué sur about');
-            homePageBody.classList.remove('hidden');
-            projectPage.classList.add('hidden');
-            homePageBody.classList.add('slide-down');
-            aboutPage.classList.add('slide-down');
-            
-            aboutClicked = false;
+//         if (clickedAbout) {
+//             console.log('je clique sur project après avoir cliqué sur about');
+//             aboutPage.classList.add('slide-up2');
+//             homePage.classList.add('slide-up');
+//             aboutPage.classList.remove('hidden');
 
-            setTimeout(function() {
-                document.title = 'Portefolio DESTEPHEN - Home';
-                setTimeout(function() {
-                    aboutPage.classList.add('hidden');
-                    setTimeout(function() {
-                        homePageBody.classList.remove('slide-down');
-                        aboutPage.classList.remove('slide-down');
-                        navbar.classList.add('homeLink-clicked');
-                    }, 1000);                
-                }, 1000);                         
-            }, 3);        
-        } else if (projectClicked) {
-            console.log('je clique sur home apres avoir cliqué sur projet');
-            homePageBody.classList.remove('hidden');
-            aboutPage.classList.add('hidden');
-            homePageBody.classList.add('slide-down');
-            projectPage.classList.add('slide-down');
- 
-            projectClicked = false;
+//             projectPage.classList.remove('slide-up3');
+//             projectPage.classList.remove('hidden');
+//             navbar.classList.add('projectLink-clicked');
+//             setTimeout(function() {
+                
+//                 projectPage.classList.add('slide-up');
+//                 setTimeout(function() {
+//                     aboutPage.classList.add('hidden');
+//                     aboutPage.classList.remove('slide-up2');
+//                 }, 1000);
+//             }, 3);
+//         } else if (clickedHome) {
+//             console.log('je clique sur project après avoir cliqué sur home');
+//             aboutPage.classList.add('slide-up');
+//             homePage.classList.add('slide-up');
+//             aboutPage.classList.add('hidden');
 
-            setTimeout(function() {
-                document.title = 'Portefolio DESTEPHEN - Home';
-                setTimeout(function() {
-                    setTimeout(function() {
-                        projectPage.classList.add('hidden');
-                        homePageBody.classList.remove('slide-down');
-                        projectPage.classList.remove('slide-down');
-                        navbar.classList.add('homeLink-clicked');
-                    }, 1000);                
-                }, 1000);                         
-            }, 3);        
-        } else {
-            console.log('je clique sur home directement');
-            aboutPage.classList.remove('slide-down2');
-            projectPage.classList.remove('slide-down3');
-            homePageBody.classList.add('slide-down');
-            projectPage.classList.add('slide-down');
-            aboutPage.classList.add('slide-down');
+//             projectPage.classList.remove('slide-up3');
+//             projectPage.classList.remove('hidden');
+//             navbar.classList.add('projectLink-clicked');
+//             setTimeout(function() {
+//                 projectPage.classList.add('slide-up');
+//             }, 3);
+//         } else {
+//             console.log('je clique sur project directement');
+//             aboutPage.classList.add('slide-up');
+//             homePage.classList.add('slide-up');
+//             aboutPage.classList.add('hidden');
 
-            setTimeout(function() {
-                setTimeout(function() {
-                    homePageBody.classList.remove('hidden');
-                    aboutPage.classList.add('hidden');
-                    projectPage.classList.add('hidden');
-                    navbar.classList.add('homeLink-clicked');
-                }, 1000);
-            }, 3);
+//             projectPage.classList.remove('slide-up3');
+//             projectPage.classList.remove('hidden');
+//             navbar.classList.add('projectLink-clicked');
+//             setTimeout(function() {
+//                 projectPage.classList.add('slide-up');
+//             }, 3);
+//         }
 
-            homeClicked = true;
-        }
-    });
-}
-
-if (aboutLink) {
-    aboutLink.addEventListener('click', function(about) {
-        about.preventDefault();
-
-        aboutResetCSS()
-
-        if (homeClicked) {
-            console.log('je clique sur about apres avoir cliqué sur home');
-            homePageBody.classList.add('slide-up');
-            
-            homeClicked = false;
-
-            setTimeout(function() {
-                aboutPage.classList.add('slide-up'); 
-                document.title = 'Portefolio DESTEPHEN - A propos';
-                navbar.classList.add('aboutLink-clicked');
-            }, 3);
-        } else if (projectClicked) {
-            console.log('je clique sur about apres avoir cliqué sur projet');
-            projectPage.classList.remove('slide-up3');
-            projectPage.classList.remove('hidden');
-            projectPage.classList.add('slide-down3');
-            homePageBody.classList.add('hidden');
-            homePageBody.classList.add('slide-up');
-            aboutPage.classList.add('slide-up2');
-            
-            projectClicked = false;
-
-            setTimeout(function() {
-                aboutPage.classList.add('slide-down2');
-                document.title = 'Portefolio DESTEPHEN - A propos';
-                setTimeout(function() {
-                    aboutPage.classList.remove('hidden');
-                    aboutPage.classList.remove('slide-up2');
-                    projectPage.classList.add('hidden');
-                    navbar.classList.add('aboutLink-clicked');
-                }, 1000);                   
-            }, 3);
-        } else {
-            console.log('je clique sur about directement');
-            projectPage.classList.add('hidden');
-            homePageBody.classList.add('slide-up');
-            setTimeout(function() {
-                aboutPage.classList.add('slide-up');
-                document.title = 'Portefolio DESTEPHEN - A propos';
-                navbar.classList.add('aboutLink-clicked');
-            }, 3);
-    
-            aboutClicked = true;
-        }
-    });
-}
-
-if (projectLink) {
-    projectLink.addEventListener('click', function(project) {
-        project.preventDefault();
-
-        projectResetCSS()
-
-        if (aboutClicked) {
-            console.log('je clique sur projet apres avoir cliqué sur about');
-            aboutPage.classList.remove('hidden');
-            homePageBody.classList.add('hidden');
-            projectPage.classList.remove('hidden');
-            aboutPage.classList.add('slide-up2');
-
-            aboutClicked = false;
-
-            setTimeout(function() {
-                projectPage.classList.add('slide-up3');
-                document.title = 'Portefolio DESTEPHEN - Mes projets';
-                setTimeout(function() {
-                    aboutPage.classList.add('hidden');
-                    setTimeout(function() {
-                        aboutPage.classList.remove('slide-up2');
-                        navbar.classList.add('projectLink-clicked');
-                    }, 1000);
-                }, 1000); 
-            }, 3);
-        } else if (homeClicked) {
-            console.log('je clique sur projet apres avoir cliqué sur home');
-            aboutPage.classList.add('slide-up');
-            homePageBody.classList.add('slide-up');
-            projectPage.classList.remove('hidden');
-            navbar.classList.add('projectLink-clicked');
-
-            setTimeout(function() {
-                projectPage.classList.add('slide-up');
-                document.title = 'Portefolio DESTEPHEN - Mes projets';
-            }, 3);
-            homeClicked = false;
-            aboutClicked = false;
-        } else {
-            console.log('je clique sur projet directement');
-            aboutPage.classList.add('slide-up');
-            homePageBody.classList.add('slide-up');
-            projectPage.classList.remove('slide-up3');
-            projectPage.classList.remove('hidden');
-            navbar.classList.add('projectLink-clicked');
-
-            setTimeout(function() {
-                projectPage.classList.add('slide-up');
-                document.title = 'Portefolio DESTEPHEN - Mes projets';
-            }, 3);
-
-            projectClicked = true;
-        }
-    });
-}
+//         console.log(userHistory);
+//     });
+// }
